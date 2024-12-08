7b0554f042bee15b384601b37fe03c47c808068b
Fernando <fernando-ravelli@hotmail.com>
2024-12-08 18:53:09 -0300

    flow json

--- obj/Debug/net6.0/BlipChat.AssemblyInfo.cs
+++ obj/Debug/net6.0/BlipChat.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlipChat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58e1cd6fa9217741df5456238e26651fd7696111")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+016f1677f4a82b873bcf3cfda1437281b3b24af3")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlipChat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlipChat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
