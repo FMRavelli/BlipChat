58e1cd6fa9217741df5456238e26651fd7696111
Fernando <fernando-ravelli@hotmail.com>
2024-12-08 18:43:58 -0300

    Deploy

--- obj/Debug/net6.0/BlipChat.AssemblyInfo.cs
+++ obj/Debug/net6.0/BlipChat.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlipChat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f958e7a4f94355a245be9e0f2d63769cec0648db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07e0abe7c94adcf9bebfd6dbfb0aeac315cdb04c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlipChat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlipChat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
